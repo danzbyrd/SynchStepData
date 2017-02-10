f8d907ae4fc01b34e86827a99746c1713a3ee956
danzb <danzbyrd@gmail.com>
2017-02-10 00:48:56 -0500

    Added Spring REST and followed example on Springs Tutorial on consuming a RESTFUL service.

---
+++ app/src/main/java/com/health/danzb/synchstepdata/Greeting.java
@@ -0,0 +1,27 @@
+package com.health.danzb.synchstepdata;
+
+/**
+ * Created by danzb on 2/10/2017.
+ */
+
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
